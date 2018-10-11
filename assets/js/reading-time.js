e92a828720dc1425341a0319f1d8148bd5571007
Tim Bennett <2475478+bennetimo@users.noreply.github.com>
2018-10-11 10:52:38 +0100

    more small tweaks

--- assets/js/reading-time.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var txt = $('.content')[0].textContent,
-    wordCount = txt.replace( /[^\w ]/g, "" ).split( /\s+/ ).length;
-
-    var readingTimeInMinutes = Math.floor(wordCount / 250) + 1;
-    var readingTimeAsString = readingTimeInMinutes + " min read";
-    $('article .reading-time').html(readingTimeAsString);
-});
